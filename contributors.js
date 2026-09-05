d1100eee5df3ebfeca76c2e964ff30f4a5473142
bdinka-tomas <bdinkatomas@gmail.com>
2026-09-05 12:17:15 +0000

    Create contributors.js

---
+++ contributors.js
@@ -0,0 +1,7 @@
+const contributorsData = [
+    {
+        name: "bdinkatomas-dev",
+        logo: "https://avatars.githubusercontent.com/u/264227267?v=4",
+        url: "https://github.com/bdinkatomas-dev"
+    },
+];

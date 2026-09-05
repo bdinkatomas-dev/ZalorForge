bcb8951fc8544a420a50d90df10a715659b86407
bdinka-tomas <bdinkatomas@gmail.com>
2026-09-05 19:20:40 +0000

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,6 +2,6 @@ const appsData = [
     {
         name: "Check",
         logo: "zalorforge.pages.dev/uploads/check.svg",
-        url: "zalorforge.pages.dev/check/"
+        url: "/check/"
     },
 ];
